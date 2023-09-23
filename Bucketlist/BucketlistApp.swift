5e1f106180b1e40214fa9b8d03f2e0ebca682fcf
Ojidaemo <vit.martsinovich@gmail.com>
2023-09-23 12:39:52 +0300

    Initial Commit

---
+++ Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by Vitali Martsinovich on 2023-09-23.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
